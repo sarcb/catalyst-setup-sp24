d30f83381f5e89bd78d8c866bb107a351ca70630
Jonas Plum <114533931+jonas-plum@users.noreply.github.com>
2023-02-05 03:32:50 +0100

    Update to v0.10.3

--- install_catalyst.sh
+++ install_catalyst.sh
@@ -116,7 +116,7 @@ INITIAL_API_KEY=$(openssl rand -hex 64)
 echo "$INITIAL_API_KEY" >INITIAL_API_KEY
 
 # download catalyst setup
-curl -sL "https://github.com/SecurityBrewery/catalyst-setup/archive/refs/tags/v0.10.1.zip" -o catalyst_install.zip
+curl -sL "https://github.com/SecurityBrewery/catalyst-setup/archive/refs/tags/v0.10.3.zip" -o catalyst_install.zip
 unzip catalyst_install.zip
 cd "catalyst-setup-0.10.1"
 
